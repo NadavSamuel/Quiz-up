c11e9c9b1e6935870bf168afaa10e9bd9f1fcbc5
talah221 <68655471+talah221@users.noreply.github.com>
2020-09-16 19:54:15 +0300

    quizDetails basic korkikent athala simple ragua

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import { QuizApp } from './pages/QuizApp'
+import { QuizDetails } from './pages/QuizDetails';
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route component={ QuizDetails } path='/quiz/:quizId' />
         <Route component={ QuizApp } path='/' />
       </Switch>
     </div>
