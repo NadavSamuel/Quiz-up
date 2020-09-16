b774dbca83cdafe1361b38059f6427f2fcd81121
talah221 <68655471+talah221@users.noreply.github.com>
2020-09-16 17:02:24 +0300

    minor changes

--- src/store/store.js
+++ src/store/store.js
@@ -10,3 +10,5 @@ const rootReducer = combineReducers({
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
+console.log('shinui')
