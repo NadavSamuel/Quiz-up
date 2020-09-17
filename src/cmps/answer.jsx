2685f413bb5d9fc059cf8615da97b805cf244403
NadavSamuel <68653661+NadavSamuel@users.noreply.github.com>
2020-09-17 09:12:02 +0300

    basi endgame pagec

--- src/cmps/answer.jsx
+++ src/cmps/answer.jsx
@@ -4,7 +4,7 @@ export function Answer({ answer,idx,answerQuestion,answerFeedback }) {
     console.log("AnswerList -> answer", answer,idx)
     const answerColor = answer.isCorrect === 'true'? 'green':'red'
     return (
-        <div className={"answer-"+idx}>
+        <div className={"answer answer-"+idx}>
             <h3 style={answerFeedback?{color: answerColor}:{color: ''}} onClick={() =>answerQuestion(answer.isCorrect)}>{answer.txt}</h3>
 
         </div>
