9c7cadf375c40397830aa8a391e75d0430f7203f
talah221 <68655471+talah221@users.noreply.github.com>
2020-09-22 14:06:37 +0300

    14

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -1,3 +1,8 @@
+// module.exports = {
+//   "dbURL": "mongodb://localhost:27017",
+// }
+
+
 module.exports = {
-  "dbURL": "mongodb://localhost:27017",
+  "dbURL": "mongodb+srv://tal:talp123@cluster0.hy1hx.mongodb.net/test?retryWrites=true&w=majority",
 }
