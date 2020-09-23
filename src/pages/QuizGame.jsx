56f769c06cb9bd0df677a222f89373e3e52531ab
NadavSamuel <68653661+NadavSamuel@users.noreply.github.com>
2020-09-23 13:21:28 +0300

    another stupid bug fix

--- src/pages/QuizGame.jsx
+++ src/pages/QuizGame.jsx
@@ -54,7 +54,7 @@ class _QuizGame extends Component {
                 _id: utilService.makeId(),
             }, isSetName: false
         }, () => {
-            this.timer = setInterval(this.setTimer, 15000)
+            this.timer = setInterval(this.setTimer, 1000)
         })
     }
     // getRandomUserObject = () =>{
