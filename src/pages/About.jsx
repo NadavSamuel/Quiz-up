6bf66643cf45402382446df0dcfb62209ef40b23
talah221 <68655471+talah221@users.noreply.github.com>
2020-09-26 21:44:03 +0300

    some

---
+++ src/pages/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <div className="quizup-story">
+                <h1>The Story</h1>
+                <p>
+                    "Quiz-Up" Founded in 2020 as part of the "Coding-Academy" course, as part of the final project.
+                     The main purpose of the app is to allow you to create quizes, 
+                    play  together and even share and play with friends on the questionnaires you have created.
+                </p>
+            </div>
+            <div className="technologies"></div>
+            <div className="creators"></div>
+            <div className="thanks"></div>
+        </React.Fragment>
+
+
+    )
+}
